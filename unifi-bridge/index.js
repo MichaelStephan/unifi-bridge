04bd5db4d330e9010c38b2714183e70c5ff036d4
Michael Stephan <4299762+MichaelStephan@users.noreply.github.com>
2023-07-31 21:04:32 +0200

    1.0.7

--- unifi-bridge/index.js
+++ unifi-bridge/index.js
@@ -73,25 +73,32 @@ if (port == undefined) {
 
 const unifi = new ExtendedUnifiController({host, port, sslverify: false});
 
+webservice.get('/firewall_rule/:rule_set/:rule_id/enabled', async (req, res) => {
+	var rule_set = req.params['rule_set'] 
+	var rule_id = req.params['rule_id'] 
+
+	await unifi.login(username, password);
+	firewall_rule = await unifi.getFirewallRule(rule_set, rule_id)
+	if (firewall_rule == null) {
+		res.status(404)
+		res.end()
+	} else {
+		res.json(firewall_rule.enabled)
+	}
+})
+
 webservice.get('/firewall_rule/:rule_set/:rule_id', async (req, res) => {
 	var rule_set = req.params['rule_set'] 
 	var rule_id = req.params['rule_id'] 
 
-	try {
-		await unifi.login(username, password);
-		firewall_rule = await unifi.getFirewallRule(rule_set, rule_id)
-		if (firewall_rule == null) {
-			res.status(404)
-			res.end()
-		}
-		else {
-			res.json(firewall_rule)
-		}
-	} catch(e) {
-		console.log(e)
-		res.status(500)
+	await unifi.login(username, password);
+	firewall_rule = await unifi.getFirewallRule(rule_set, rule_id)
+	if (firewall_rule == null) {
+		res.status(404)
 		res.end()
-	}	
+	} else {
+		res.json(firewall_rule)
+	}
 })
 
 webservice.post('/firewall_rule/:rule_set/:rule_id/toggle', express.json(), async (req, res) => {
