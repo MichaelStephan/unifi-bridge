efd8d3e280dd092e2b31368419ecdb8d3b1e58c9
Michael Stephan <4299762+MichaelStephan@users.noreply.github.com>
2023-08-02 22:20:59 +0200

    adding mqtt support

--- unifi-bridge/index.js
+++ unifi-bridge/index.js
@@ -1,9 +1,39 @@
-const express = require("express")
-const Unifi = require('node-unifi');
+const express = require('express')
+const mqtt = require('mqtt');
+const Unifi = require('node-unifi')
 
-console.log('Initializing webservice.')
+function must_have_env(variable, name) {
+	if (variable == null || variable.trim().length == 0) {
+		console.log(`You must set ${name} in the addon config`)
+		process.exit(22)
+	}
+	return variable
+}
+
+unifi_config = {
+	username : must_have_env(process.env.UNIFI_USERNAME, 'unifi_username'),
+	password : must_have_env(process.env.UNIFI_PASSWORD, 'unifi_password'),
+	site : must_have_env(process.env.UNIFI_SITE, 'unifi_site'),
+	host : must_have_env(process.env.UNIFI_HOST, 'unifi_host'),
+	port : must_have_env(process.env.UNIFI_PORT, 'unifi_port')
+}
+
+mqtt_config = {
+	username : must_have_env(process.env.MQTT_USERNAME, 'mqtt_username'),
+	password : must_have_env(process.env.MQTT_PASSWORD, 'mqtt_password'),
+	endpoint : must_have_env(process.env.MQTT_ENDPOINT, 'mqtt_endpoint'),
+	topic_base : 'unifi-bridge'
+}
+
+console.log('Initializing mqtt connection')
+const mqtt_client = mqtt.connect(mqtt_config.endpoint)
+
+mqtt_client.on('connect', () => {
+	console.log('Mqtt connection established')
+});
+
+console.log('Initializing webservice')
 const webservice = express()
-
 
 class ExtendedUnifiController extends Unifi.Controller {
   enableFirewallRule(ruleSet, ruleIndex) {
@@ -33,45 +63,21 @@ class ExtendedUnifiController extends Unifi.Controller {
   async getFirewallRule(ruleSet, ruleIndex) {
     const firewallRules = await this.getFirewallRules()
     const rule = firewallRules.find(rule => rule.ruleset == ruleSet && rule.rule_index == ruleIndex)
-    return rule
-  }
+	return rule
+}
 
   async editFirewallRule(rule) {
-    return this._request('/api/s/<SITE>/rest/firewallrule/' + rule._id, rule, 'PUT');
+    const rules = await this._request('/api/s/<SITE>/rest/firewallrule/' + rule._id, rule, 'PUT');
+	if (rules.length > 0) {
+		return rules[0]
+	} else {
+		return null
+	}
   }
 }
 
-var username = process.env.USERNAME
-if (username == undefined) {
-	console.log("You must set an user in the addon config")
-	process.exit(22)
-}
 
-var password = process.env.PASSWORD
-if (password == undefined) {
-	console.log("You must set a password in the addon config")
-	process.exit(22)
-}
-
-var site = process.env.SITE
-if (site == undefined) {
-	console.log("You must set a site in the addon config")
-	process.exit(22)
-}
-
-var host = process.env.HOST
-if (host == undefined) {
-	console.log("You must set an host in the addon config")
-	process.exit(22)
-}
-
-var port = process.env.PORT
-if (port == undefined) {
-	console.log("You must set a port in the addon config")
-	process.exit(22)
-}
-
-const unifi = new ExtendedUnifiController({host, port, sslverify: false});
+const unifi = new ExtendedUnifiController({host: unifi_config.host, port: unifi_config.port, sslverify: false, timeout: 30000});
 
 webservice.get('/firewall_rule/:rule_set/:rule_id', async (req, res) => {
 	var rule_set = req.params['rule_set'] 
@@ -79,20 +85,18 @@ webservice.get('/firewall_rule/:rule_set/:rule_id', async (req, res) => {
 
 	firewall_rule = await unifi.getFirewallRule(rule_set, rule_id)
 	if (firewall_rule == null) {
-		res.status(404)
-		res.end()
+		res.status(404).end()
 	} else {
-		res.json(firewall_rule)
+		res.json(firewall_rule).end()
 	}
 })
 
 webservice.get('/client_devices', async (req, res) => {
 	client_devices = await unifi.getClientDevices()
 	if (client_devices == null) {
-		res.status(404)
-		res.end()
+		res.status(404).end()
 	} else {
-		res.json(client_devices)
+		res.json(client_devices).end()
 	}
 })
 
@@ -102,17 +106,17 @@ webservice.post('/firewall_rule/:rule_set/:rule_id/toggle', express.json(), asyn
 
 	firewall_rule = await unifi.toggleFirewallRule(rule_set, rule_id)
 	if (firewall_rule == null) {
-		res.status(404)
-		res.end()
+		res.status(404).end()
 	}
 	else {
-		res.json(firewall_rule)
+		mqtt_client.publish(`${mqtt_config.topic_base}/${unifi_config.site}/firewall_rule/${rule_set}/${rule_id}/enabled`, JSON.stringify(firewall_rule.enabled));
+		res.json(firewall_rule).end()
 	}	
 })
 
-console.log(`Starting listener on port ${port}.`)
+console.log(`Starting listener on port 8000`)
 webservice.listen(8000, async () => {
 	console.log('running version 1.0.11')
-	await unifi.login(username, password);
-	console.log(`Unifi Bridge is running on port ${port}.`);
+	await unifi.login(unifi_config.username, unifi_config.password);
+	console.log(`Unifi Bridge is running on port ${unifi_config.port}`);
 })
--- unifi-bridge/run.sh
+++ unifi-bridge/run.sh
@@ -1,20 +1,29 @@
 #!/usr/bin/with-contenv bashio
 set +u
 
-export USERNAME=$(bashio::config 'username')
-bashio::log.info "Username configured as ${USERNAME}."
+export UNIFI_USERNAME=$(bashio::config 'unifi_username')
+bashio::log.info "Unifi username configured as ${UNIFI_USERNAME}."
 
-export PASSWORD=$(bashio::config 'password')
-bashio::log.info "Password configured as ********."
+export UNIFI_PASSWORD=$(bashio::config 'unifi_password')
+bashio::log.info "Unifi password configured as ********."
 
-export SITE=$(bashio::config 'site')
-bashio::log.info "Site configured as ${SITE}."
+export UNIFI_SITE=$(bashio::config 'unifi_site')
+bashio::log.info "Unifi site configured as ${UNIFI_SITE}."
 
-export HOST=$(bashio::config 'host')
-bashio::log.info "Host configured as ${HOST}."
+export UNIFI_HOST=$(bashio::config 'unifi_host')
+bashio::log.info "Unifi host configured as ${UNIFI_HOST}."
 
-export PORT=$(bashio::config 'port')
-bashio::log.info "Port configured as ${PORT}."
+export UNIFI_PORT=$(bashio::config 'unifi_port')
+bashio::log.info "Unifi port configured as ${UNIFI_PORT}."
+
+export MQTT_ENDPOINT=$(bashio::config 'mqtt_endpoint')
+bashio::log.info "MQTT endpoint as ${MQTT_ENDPOINT}."
+
+export MQTT_USERNAME=$(bashio::config 'mqtt_username')
+bashio::log.info "MQTT username as ${MQTT_USERNAME}."
+
+export MQTT_PASSWORD=$(bashio::config 'mqtt_password')
+bashio::log.info "MQTT password configured as ********."
 
 bashio::log.info "Starting bridge service."
 npm run start
