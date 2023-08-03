dfab21f774acaca3dd825c112b09188c1fc95956
Michael Stephan <4299762+MichaelStephan@users.noreply.github.com>
2023-08-03 07:43:56 +0200

    propagating mqtt credentials on login; bumping version to 1.0.14

--- unifi-bridge/run.sh
+++ unifi-bridge/run.sh
@@ -2,28 +2,28 @@
 set +u
 
 export UNIFI_USERNAME=$(bashio::config 'unifi_username')
-bashio::log.info "Unifi username configured as ${UNIFI_USERNAME}."
+bashio::log.info "Unifi username configured as ${UNIFI_USERNAME}"
 
 export UNIFI_PASSWORD=$(bashio::config 'unifi_password')
-bashio::log.info "Unifi password configured as ********."
+bashio::log.info "Unifi password configured as ********"
 
 export UNIFI_SITE=$(bashio::config 'unifi_site')
-bashio::log.info "Unifi site configured as ${UNIFI_SITE}."
+bashio::log.info "Unifi site configured as ${UNIFI_SITE}"
 
 export UNIFI_HOST=$(bashio::config 'unifi_host')
-bashio::log.info "Unifi host configured as ${UNIFI_HOST}."
+bashio::log.info "Unifi host configured as ${UNIFI_HOST}"
 
 export UNIFI_PORT=$(bashio::config 'unifi_port')
-bashio::log.info "Unifi port configured as ${UNIFI_PORT}."
+bashio::log.info "Unifi port configured as ${UNIFI_PORT}"
 
 export MQTT_ENDPOINT=$(bashio::config 'mqtt_endpoint')
-bashio::log.info "MQTT endpoint as ${MQTT_ENDPOINT}."
+bashio::log.info "MQTT endpoint as ${MQTT_ENDPOINT}"
 
 export MQTT_USERNAME=$(bashio::config 'mqtt_username')
-bashio::log.info "MQTT username as ${MQTT_USERNAME}."
+bashio::log.info "MQTT username as ${MQTT_USERNAME}"
 
 export MQTT_PASSWORD=$(bashio::config 'mqtt_password')
-bashio::log.info "MQTT password configured as ********."
+bashio::log.info "MQTT password configured as ********"
 
-bashio::log.info "Starting bridge service."
+bashio::log.info "Starting bridge service"
 npm run start
