050da98a33cfaf149eb3b91512de614d71aa3196
Michael Stephan <4299762+MichaelStephan@users.noreply.github.com>
2023-08-06 13:58:30 +0200

    bumping version

--- unifi-bridge/run.sh
+++ unifi-bridge/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/with-contenv bashio
 set +u
 
-bashio::log.info "Running version 1.0.36"
+bashio::log.info "Running version 1.0.37"
 
 export UNIFI_USERNAME=$(bashio::config 'unifi_username')
 bashio::log.info "Unifi username configured as ${UNIFI_USERNAME}"
